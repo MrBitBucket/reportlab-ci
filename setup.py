4ea27877504fbdad0a2f440c7cb81eda5938f837
replabrobin <robin@reportlab.com>
2022-03-14 15:59:30 +0000

    protect against missing ARCHFLAGS

--- setup.py
+++ setup.py
@@ -224,7 +224,7 @@ class inc_lib_dirs:
             aDir(L, os.path.join("/usr/lib", "python%s" % sys.version[:3], "config"))
         elif platform == "darwin":
             machine = sysconfig_platform.split('-')[-1]
-            if machine=='arm64' or os.environ['ARCHFLAGS']=='-arch arm64':
+            if machine=='arm64' or os.environ.get('ARCHFLAGS','')=='-arch arm64':
                 #print('!!!!! detected darwin arm64 build')
                 #probably an M1
                 target = pjoin(
