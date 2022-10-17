712769c7961c88cf0abf953ba4013e7ea38b1456
replabrobin <robin@reportlab.com>
2022-10-17 12:34:02 +0100

    testing freetype-py FTTextPath possibility

--- src/reportlab/graphics/utils.py
+++ src/reportlab/graphics/utils.py
@@ -47,21 +47,22 @@ def pathNumTrunc(n):
     return round(n,5)
 
 
-def __makeTextPathsCode__(tp=None):
+def __makeTextPathsCode__(tp=None, _TP = ('freetype','_renderPM')):
     from reportlab.rl_config import textPaths, renderPMBackend
     if tp is not None: textPaths = tp
     if textPaths=='backend':
         tp = 'freetype' if renderPMBackend!='_renderPM' else '_renderPM'
-    elif textPaths in ('freetype','_renderPM'):
+    elif textPaths in _TP:
         tp = textPaths
     else:
         raise ValueError(f"textPaths={textPaths!r} should be one of 'backend', 'freetype' or '_renderPM')")
-    if tp=='freetype':
-        try:
-            import freetype
-        except ImportError:
-            tp = '_renderPM'
-        else:
+    TP = (tp,) + tuple((_ for _ in _TP if _!=tp))
+    for tp in TP:
+        if tp=='freetype':
+            try:
+                import freetype
+            except ImportError:
+                continue
             import io
             class FTTextPath:
                 ftLFlags = freetype.FT_LOAD_DEFAULT | freetype.FT_LOAD_NO_SCALE | freetype.FT_LOAD_NO_BITMAP
@@ -135,116 +136,120 @@ def __makeTextPathsCode__(tp=None):
                     if P: P_append(('closePath',))
                     return P
 
-        def text2PathDescription(text, x=0, y=0, fontName=_baseGFontName, fontSize=1000,
-                                    anchor='start', truncate=1, pathReverse=0, gs=None):
-            '''freetype text2PathDescription(text, x=0, y=0, fontName='fontname',
-                                fontSize=1000, font = 'fontName',
-                                anchor='start', truncate=1, pathReverse=0, gs=None)
-            '''
-            font = getFont(fontName)
-            if font._multiByte and not font._dynamicFont:
-                raise ValueError("text2PathDescription doesn't support multi byte fonts like %r" % fontName)
-            P_extend = [].extend
-            if not anchor=='start':
-                textLen = stringWidth(text, fontName, fontSize)
-                if anchor=='end':
-                    x = x-textLen
-                elif anchor=='middle':
-                    x = x - textLen/2.
-            if gs is None:
-                gs = FTTextPath()
-            if font._dynamicFont:
-                P_extend(gs._text2Path(text,x=x,y=y,fontName=fontName,fontSize=fontSize, truncate=truncate,pathReverse=pathReverse))
-            else:
-                if isBytes(text):
-                    try:
-                        text = text.decode('utf8')
-                    except UnicodeDecodeError as e:
-                        i,j = e.args[2:4]
-                        raise UnicodeDecodeError(*(e.args[:4]+('%s\n%s-->%s<--%s' % (e.args[4],text[max(i-10,0):i],text[i:j],text[j:j+10]),)))
-                FT = unicode2T1(text,[font]+font.substitutionFonts)
-                nm1 = len(FT)-1
-                for i, (f, t) in enumerate(FT):
-                    if isinstance(t,bytes): t = t.decode(f.encName)
-                    P_extend(gs._text2Path(t,x=x,y=y,fontName=f.fontName,fontSize=fontSize, truncate=truncate,pathReverse=pathReverse))
-                    if i!=nm1:
-                        x += f.stringWidth(t, fontSize)
-            return P_extend.__self__
-        return dict(text2PathDescription=text2PathDescription,FTTextPath=FTTextPath)
+            def text2PathDescription(text, x=0, y=0, fontName=_baseGFontName, fontSize=1000,
+                                        anchor='start', truncate=1, pathReverse=0, gs=None):
+                '''freetype text2PathDescription(text, x=0, y=0, fontName='fontname',
+                                    fontSize=1000, font = 'fontName',
+                                    anchor='start', truncate=1, pathReverse=0, gs=None)
+                '''
+                font = getFont(fontName)
+                if font._multiByte and not font._dynamicFont:
+                    raise ValueError("text2PathDescription doesn't support multi byte fonts like %r" % fontName)
+                P_extend = [].extend
+                if not anchor=='start':
+                    textLen = stringWidth(text, fontName, fontSize)
+                    if anchor=='end':
+                        x = x-textLen
+                    elif anchor=='middle':
+                        x = x - textLen/2.
+                if gs is None:
+                    gs = FTTextPath()
+                if font._dynamicFont:
+                    P_extend(gs._text2Path(text,x=x,y=y,fontName=fontName,fontSize=fontSize, truncate=truncate,pathReverse=pathReverse))
+                else:
+                    if isBytes(text):
+                        try:
+                            text = text.decode('utf8')
+                        except UnicodeDecodeError as e:
+                            i,j = e.args[2:4]
+                            raise UnicodeDecodeError(*(e.args[:4]+('%s\n%s-->%s<--%s' % (e.args[4],text[max(i-10,0):i],text[i:j],text[j:j+10]),)))
+                    FT = unicode2T1(text,[font]+font.substitutionFonts)
+                    nm1 = len(FT)-1
+                    for i, (f, t) in enumerate(FT):
+                        if isinstance(t,bytes): t = t.decode(f.encName)
+                        P_extend(gs._text2Path(t,x=x,y=y,fontName=f.fontName,fontSize=fontSize, truncate=truncate,pathReverse=pathReverse))
+                        if i!=nm1:
+                            x += f.stringWidth(t, fontSize)
+                return P_extend.__self__
+            return dict(text2PathDescription=text2PathDescription,FTTextPath=FTTextPath)
+        elif tp=='_renderPM':
+            try:
+                from . import _renderPM
+            except ImportError:
+                continue
+            def processGlyph(G, truncate=1, pathReverse=0):
+                O = []
+                P = []
+                R_append = [].append
+                if G and len(G)==1 and G[0][0]=='lineTo':
+                    G = (('moveToClosed',)+G[0][1:],)+G #hack fix for some errors
+                for g in (G or ())+(('end',),):
+                    op = g[0]
+                    if O and op in ['moveTo', 'moveToClosed','end']:
+                        if O[0]=='moveToClosed':
+                            del O[0]
+                            if pathReverse:
+                                P[1::2],P[0::2] = P[0::2],P[1::2]   #exchange x and y
+                                P.reverse()
+                                O.reverse()
+                            O.insert(0,'moveTo')
+                            O.append('closePath')
+                        i = 0
+                        if truncate: P = list(map(pathNumTrunc,P))
+                        for o in O:
+                            j = i + _PATH_OP_ARG_COUNT[_PATH_OP_NAMES.index(o)]
+                            R_append((o,)+ tuple(P[i:j]))
+                            i = j
+                        O = []
+                        P = []
+                    O.append(op)
+                    P.extend(g[1:])
+                return R_append.__self__
 
-    if tp=='_renderPM':
-        def processGlyph(G, truncate=1, pathReverse=0):
-            O = []
-            P = []
-            R_append = [].append
-            if G and len(G)==1 and G[0][0]=='lineTo':
-                G = (('moveToClosed',)+G[0][1:],)+G #hack fix for some errors
-            for g in (G or ())+(('end',),):
-                op = g[0]
-                if O and op in ['moveTo', 'moveToClosed','end']:
-                    if O[0]=='moveToClosed':
-                        del O[0]
-                        if pathReverse:
-                            P[1::2],P[0::2] = P[0::2],P[1::2]   #exchange x and y
-                            P.reverse()
-                            O.reverse()
-                        O.insert(0,'moveTo')
-                        O.append('closePath')
-                    i = 0
-                    if truncate: P = list(map(pathNumTrunc,P))
-                    for o in O:
-                        j = i + _PATH_OP_ARG_COUNT[_PATH_OP_NAMES.index(o)]
-                        R_append((o,)+ tuple(P[i:j]))
-                        i = j
-                    O = []
-                    P = []
-                O.append(op)
-                P.extend(g[1:])
-            return R_append.__self__
-
-        def text2PathDescription(text, x=0, y=0, fontName=_baseGFontName, fontSize=1000,
-                                    anchor='start', truncate=1, pathReverse=0, gs=None):
-            '''_renderPM text2PathDescription(text, x=0, y=0, fontName='fontname',
-                                fontSize=1000, font = 'fontName',
-                                anchor='start', truncate=1, pathReverse=0, gs=None)
-            '''
-            font = getFont(fontName)
-            if font._multiByte and not font._dynamicFont:
-                raise ValueError("text2PathDescription doesn't support multi byte fonts like %r" % fontName)
-            P_extend = [].extend
-            if not anchor=='start':
-                textLen = stringWidth(text, fontName, fontSize)
-                if anchor=='end':
-                    x = x-textLen
-                elif anchor=='middle':
-                    x = x - textLen/2.
-            if gs is None:
-                from ._renderPM import gstate
-                gs = gstate(1,1)
-            setFont(gs,fontName,fontSize)
-            if font._dynamicFont:
-                for g in gs._stringPath(text,x,y):
-                    P_extend(processGlyph(g,truncate=truncate,pathReverse=pathReverse))
-            else:
-                if isBytes(text):
-                    try:
-                        text = text.decode('utf8')
-                    except UnicodeDecodeError as e:
-                        i,j = e.args[2:4]
-                        raise UnicodeDecodeError(*(e.args[:4]+('%s\n%s-->%s<--%s' % (e.args[4],text[max(i-10,0):i],text[i:j],text[j:j+10]),)))
-                fc = font
-                FT = unicode2T1(text,[font]+font.substitutionFonts)
-                nm1 = len(FT)-1
-                for i, (f, t) in enumerate(FT):
-                    if f!=fc:
-                        setFont(gs,f.fontName,fontSize)
-                        fc = f
-                    for g in gs._stringPath(t,x,y):
+            def text2PathDescription(text, x=0, y=0, fontName=_baseGFontName, fontSize=1000,
+                                        anchor='start', truncate=1, pathReverse=0, gs=None):
+                '''_renderPM text2PathDescription(text, x=0, y=0, fontName='fontname',
+                                    fontSize=1000, font = 'fontName',
+                                    anchor='start', truncate=1, pathReverse=0, gs=None)
+                '''
+                font = getFont(fontName)
+                if font._multiByte and not font._dynamicFont:
+                    raise ValueError("text2PathDescription doesn't support multi byte fonts like %r" % fontName)
+                P_extend = [].extend
+                if not anchor=='start':
+                    textLen = stringWidth(text, fontName, fontSize)
+                    if anchor=='end':
+                        x = x-textLen
+                    elif anchor=='middle':
+                        x = x - textLen/2.
+                if gs is None:
+                    from ._renderPM import gstate
+                    gs = gstate(1,1)
+                setFont(gs,fontName,fontSize)
+                if font._dynamicFont:
+                    for g in gs._stringPath(text,x,y):
                         P_extend(processGlyph(g,truncate=truncate,pathReverse=pathReverse))
-                    if i!=nm1:
-                        x += f.stringWidth(t.decode(f.encName), fontSize)
-            return P_extend.__self__
-        return dict(text2PathDescription=text2PathDescription,processGlyph=processGlyph)
+                else:
+                    if isBytes(text):
+                        try:
+                            text = text.decode('utf8')
+                        except UnicodeDecodeError as e:
+                            i,j = e.args[2:4]
+                            raise UnicodeDecodeError(*(e.args[:4]+('%s\n%s-->%s<--%s' % (e.args[4],text[max(i-10,0):i],text[i:j],text[j:j+10]),)))
+                    fc = font
+                    FT = unicode2T1(text,[font]+font.substitutionFonts)
+                    nm1 = len(FT)-1
+                    for i, (f, t) in enumerate(FT):
+                        if f!=fc:
+                            setFont(gs,f.fontName,fontSize)
+                            fc = f
+                        for g in gs._stringPath(t,x,y):
+                            P_extend(processGlyph(g,truncate=truncate,pathReverse=pathReverse))
+                        if i!=nm1:
+                            x += f.stringWidth(t.decode(f.encName), fontSize)
+                return P_extend.__self__
+            return dict(text2PathDescription=text2PathDescription,processGlyph=processGlyph)
+    raise ImportError(f'Could not create text2PathDescription for using backends from {TP!a}')
 
 globals().update(__makeTextPathsCode__())
 
